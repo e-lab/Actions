c65f44b4a0c9fda32d4344e055e6b0142da7e3b6
Abhishek Chaurasia <abhishek.chaurasia29@hotmail.com>
2017-03-20 02:00:42 -0400

    using on rnncell

--- Models/model.py
+++ Models/model.py
@@ -19,20 +19,20 @@ class ModelDef(nn.Module):
         """
         super(ModelDef, self).__init__()
         # Output size of ResNet18
-        self.o_height = i_height//64
-        self.o_width = i_width//64
+        self.o_height = i_height // 64
+        self.o_width = i_width // 64
         self.n_layers = 1
         self.n_classes = n_classes
-        self.n_inp = 512 * self.o_height * self.o_width     # Input neurons of rnn
+        self.n_inp = 512 * self.o_height * self.o_width  # Input neurons of rnn
 
-        self.rnn = nn.RNN(self.n_inp, self.n_classes, 1, nonlinearity='tanh', batch_first=True)
+        self.rnn = nn.RNNCell(self.n_inp, self.n_classes, nonlinearity='tanh')
 
     def forward(self, x, h0):
+        hn = self.rnn(x, h0)
 
-        yt_seq, hn = self.rnn(x, h0)
-
-        return yt_seq, hn
+        return hn
 
     def init_hidden(self, bsz):
         weight = next(self.parameters()).data
-        return Variable(weight.new(self.n_layers, bsz, self.n_classes).zero_())
+
+        return Variable(weight.new(bsz, self.n_classes).zero_())
