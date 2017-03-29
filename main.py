3534f3fc442cff6d8f06525262c1eee674e6aaa3
Abhishek Chaurasia <abhishek.chaurasia29@hotmail.com>
2017-03-29 11:09:44 -0400

    error logger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Python imports
+import math
 import torch
 import torch.optim as optim
 import torch.nn as nn
@@ -12,12 +13,18 @@ from Models.model import ModelDef   # Get the model definition
 import generateData
 
 print('\033[0;0f\033[0J')
+# Color Palette
+CP_R = '\033[31m'
+CP_G = '\033[32m'
+CP_B = '\033[34m'
+CP_Y = '\033[33m'
+CP_C = '\033[0m'
 
 args = get_args()                   # Holds all the input argument
 
 seq_len = args.seq
 data_dir = args.data
-i_height, i_width = args.dim
+i_width, i_height = args.dim
 
 torch.manual_seed(args.seed)        # Set random seed manually
 if torch.cuda.is_available():
@@ -33,8 +40,17 @@ data_obj = generateData.TensorFolder(root=data_dir)
 data_loader = DataLoader(data_obj, batch_size=args.bs, shuffle=True, num_workers=args.workers)
 n_classes = len(data_obj.classes)
 
+# Load pretrained ResNet18 model
+pretrained_rn18 = models.resnet18(pretrained=True)
+# Remove last layer of pretrained network
+model_rn18 = nn.Sequential(*list(pretrained_rn18.children())[:-2])
+avg_pool = nn.AvgPool2d(3, stride=2, padding=1)
+
+# Output size of ResNet
+n_inp = 512 * math.ceil(i_height/64) * math.ceil(i_width/64)    # input neurons of RNN
+
 # Load model
-model = ModelDef(i_height, i_width, n_classes, args.rnn_type)        # Network architecture is stored here
+model = ModelDef(n_inp, n_classes, args.rnn_type)        # Network architecture is stored here
 
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.eta)
 loss_fn = nn.MSELoss()
@@ -47,12 +63,6 @@ def repackage_hidden(h):
     else:
         return tuple(repackage_hidden(v) for v in h)
 
-# Load pretrained ResNet18 model
-pretrained_rn18 = models.resnet18(pretrained=True)
-# Remove last layer of pretrained network
-model_rn18 = nn.Sequential(*list(pretrained_rn18.children())[:-2])
-avg_pool = nn.AvgPool2d(3, stride=2, padding=1)
-
 if args.cuda:
     model_rn18.cuda()
     avg_pool.cuda()
@@ -61,12 +71,17 @@ if args.cuda:
 h0 = model.init_hidden(args.bs)
 c0 = model.init_hidden(args.bs)
 
+# Error logger
+logger_bw = open(args.save + '/error_bw.log', 'w')
+logger_bw.write('{:10}'.format('Train Error'))
+logger_bw.write('\n{:-<10}'.format(''))
+
 def train(epoch):
     model.train()
+    total_error = 0
     for batch_idx, (data_batch_seq, target_batch_seq) in enumerate(data_loader):
         # Data is of the dimension: batch_size x frames x 3 x height x width
         n_frames = data_batch_seq.size(1)
-        n_inp = 512 * (i_height // 64) * (i_width // 64)    # input neurons of RNN
         # RNN input should be: batch_size x frames x neurons
         rnn_inputs = torch.FloatTensor(args.bs, n_frames, n_inp)
         h0 = model.init_hidden(args.bs)
@@ -93,6 +108,9 @@ def train(epoch):
             else:
                 h0 = repackage_hidden(h0)
                 h0 = model.forward(Variable(rnn_inputs[:, seq_idx]), h0, None)
+                loss = loss_fn(h0, Variable(target_batch_seq))
+                # Log batchwise error
+                logger_bw.write('\n{:.6f}'.format(loss.data[0]))
 
         loss = loss_fn(h0, Variable(target_batch_seq))
         loss.backward()
@@ -106,13 +124,33 @@ def train(epoch):
                 epoch, batch_idx * len(data_batch_seq), len(data_loader.dataset),
                 100. * batch_idx / len(data_loader), loss.data[0]))
 
+        total_error += loss.data[0]         # Total loss
+    total_error = total_error/math.ceil(len(data_obj)/args.bs)
+    return total_error
+
 
 def main():
     print("\n\033[94m\033[1me-Lab Gesture Recognition Training Script\033[0m\n")
+    error_log = list()
     for epoch in range(1, 100):
-        train(epoch)
-        with open(args.save + "model.net", 'wb') as f:
+        total_error = train(epoch)
+        print('{}{:-<50}{}'.format(CP_R, '', CP_C))
+        print('{}Epoch #: {}{:03} | {}Training Error: {}{:.6f}'.format(
+            CP_B, CP_C, epoch, CP_B, CP_C, total_error))
+        print('{}{:-<50}{}'.format(CP_R, '', CP_C))
+        error_log.append(total_error)
+        with open(args.save + "/model.pyt", 'wb') as f:
             torch.save(model, f)
+
+    logger_bw.close()
+
+    # Log batchwise error
+    logger = open(args.save + '/error.log', 'w')
+    logger.write('{:10}'.format('Train Error'))
+    logger.write('\n{:-<10}'.format(''))
+    for total_error in error_log:
+        logger.write('\n{:.6f}'.format(total_error))
+    logger.close()
 
 
 if __name__ == "__main__":
