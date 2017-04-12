aeed2e7725dd23136a7f556afab2424662c64029
menghan <menghanli723@gmail.com>
2017-04-11 22:34:08 -0400

    added test file

---
+++ test.py
@@ -0,0 +1,105 @@
+#! /usr/local/bin/python2
+import sys
+import os
+import time
+import cv2 
+import numpy as np
+import argparse
+import torch
+import torch.nn as nn
+import torchvision.models as models 
+import torchvision.transforms as transforms
+
+def define_and_parse_args():
+    # argument Checking
+    parser = argparse.ArgumentParser(description="Gesture Recognition Testing")
+    parser.add_argument('categories', help='text file with categories')
+    parser.add_argument('-i', '--input',  type=int, default='0', help='camera device index or file name, default 0')
+    parser.add_argument('-s', '--size', type=int, default=224, help='network input size')
+    return parser.parse_args()
+
+def cat_file():
+    # load classes file
+    categories = []
+    if hasattr(args, 'categories') and args.categories:
+        try:
+            f = open(args.categories, 'r')
+            for line in f:
+                cat = line.split(',')[0].split('\n')[0]
+                print (cat)
+                if cat != 'classes':
+                    categories.append(cat)
+            f.close()
+            print('Number of categories:', len(categories))
+        except:
+            print('Error opening file ' + args.categories)
+            quit()
+    return categories
+
+xres = 640
+yres = 480
+args = define_and_parse_args()
+categories = cat_file() # load category file
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# setup camera input:
+# 0 for default camera input or the path to your input video 
+cam = cv2.VideoCapture('/media/HDD1/Datasets/Gesture-eLab-Dataset/pinch/pinch-2.mp4')
+cam.set(3, xres)
+cam.set(4, yres)
+
+# load model: resnet18
+model = models.resnet18(pretrained=True)
+model.eval()
+softMax = nn.Softmax() 
+
+# image pre-processing functions:
+transformsImage = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # needed for pythorch ZOO models on ImageNet (stats)
+    ])
+
+while True:
+    startt = time.time()
+    ret, frame = cam.read()
+    if not ret:
+        break
+    
+    pframe = cv2.resize(frame, dsize=(args.size, args.size))
+    
+    # prepare and normalize frame for processing:
+    pframe = np.swapaxes(pframe, 0, 2)
+    pframe = np.expand_dims(pframe, axis=0)
+    pframe = transformsImage(pframe)
+    pframe = torch.autograd.Variable(pframe) # turn Tensor to variable required for pytorch processing
+    
+    # process via CNN model:
+    output = model(pframe)
+    if output is None:
+        print('no output from resNet model file')
+        break
+
+    output = softMax(output) # convert output to probabilities
+    output = output.data.numpy()[0] # get data from pytorch Variable, [0] = get vector from array
+    print(output)
+    # process output and print results:
+    order = output.argsort()
+    print(order)
+    last = len(categories)-1
+    text = ''
+    for i in range(min(5, last+1)):
+        text += categories[order[last-i]] + ' (' + '{0:.2f}'.format(output[order[last-i]]*100) + '%)' + '\n'
+
+    # overlay on GUI frame
+    cv2.putText(frame, text, (10, yres-20), font, 0.5, (255, 0, 0), 1)
+    cv2.imshow('win', frame)
+
+    endt = time.time()
+    sys.stdout.write("\rfps: "+'{0:.2f}'.format(1/(endt-startt)))
+    sys.stdout.flush()
+    
+    if cv2.waitKey(1) == 27: # ESC to stop
+        break
+# end program:
+cam.release()
+cv2.destroyAllWindows()
